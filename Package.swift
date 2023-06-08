4e16118e2761ec2a3e43daec01dd5663834ec121
MaxDI <120356115+it-senior-developer@users.noreply.github.com>
2023-06-08 14:44:19 +0300

    Update Package.swift
    AppsFlyerFramework

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework.git", from: "6.8.0"),
+        .package(url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework.git", from: "6.9.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
