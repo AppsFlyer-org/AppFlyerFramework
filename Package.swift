59cbc68173621325e3910adcd41840246b6f71e6
Aiub Duduev <aub.duduev.1989@gmail.com>
2023-06-08 16:12:48 +0300

    version

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework", from: "6.11.1"),
+        .package(url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework", branch: "6.9.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
